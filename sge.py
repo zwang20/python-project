e7747d1e3c42dc9c272f5c8cd5969f98c0c8492d
Michael <zwang20@knox.nsw.edu.au>
2018-08-31 19:46:21 +1000

    Minor changes

--- sge.py
+++ sge.py
@@ -1,12 +1,14 @@
 import pygame
 
+
 print('Simple Game Engine by Michael')
+
 
 def sge_print(display, string='Test', x=0, y=0, colour=(0, 0, 0)):
     display.blit(pygame.font.SysFont("arial",
         25).render(string, True, colour), (x, y))
 
-    
+
 def sge_rect(display, x, y, width, height, colour=(0, 0, 0)):
     if x <= 0:
         x = 1
@@ -18,8 +20,12 @@ def sge_rect(display, x, y, width, height, colour=(0, 0, 0)):
         height = 1
     pygame.draw.rect(display, colour, (x, y, width, height))
 
+
 def sge_clear(display):
     display.fill((255, 255, 255))
 
-def sge_line(display, colour=(0,0,0), point_1=(0, 0), point_2=(10, 10), width=(10)):
+
+def sge_line(
+display, colour=(0,0,0), point_1=(0, 0), point_2=(10, 10), width=(10)
+):
     pygame.draw.line(display, colour, point_1, point_2, width)
