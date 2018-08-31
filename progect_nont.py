9fa60b2ba128b78418f89c257ffe741f0cf9fb13
Michael <zwang20@knox.nsw.edu.au>
2018-08-31 14:35:02 +1000

    Minor changes

--- progect_nont.py
+++ progect_nont.py
@@ -14,13 +14,11 @@ game_display = pygame.display.set_mode([800, 800])
 # TODO: set caption
 # TODO: set icon
 
-# Use Vectors
-# a^2 + b^2 = c^2
-# lol xd
-
-pygame.mouse.set_visible(False)
+pygame.mouse.set_visible(False) # Disable Mouse
 bullets = [] # x, y, vector x, vextor y
-cooldown = 0
+cooldown = 0 # Init cooldown
+
+# Main loop
 while True:
 
     # init
@@ -47,20 +45,17 @@ while True:
             fire = True
 
     # mouse
-    # sge_rect(game_display, *mouse_pos, 10, 10)
     sge_rect(game_display, mouse_pos[0]-8, mouse_pos[1]-1, 16, 2, red)
     sge_rect(game_display, mouse_pos[0]-1, mouse_pos[1]-8, 2, 16, red)
-    # sge_line(game_display, black, (400, 400), mouse_pos)
 
-    # bullets i guess
-    # calculate the vector
-    # lol math comes in handy
-    # similar triangles amirite
+    # temp ratios
     temp = (((((mouse_pos[0]-400)**2)+((mouse_pos[1]-400)**2))**0.5)/10)
-    # print(temp)
-    
+
+    # temp ZeroDivision error
     if temp == 0:
         temp = 0.1
+
+    # Fire
     if fire and cooldown < 100 and cooldown%4 == 0:
         bullets.append(
             [400, 400, (mouse_pos[0]-400)/temp, (mouse_pos[1]-400)/temp])
@@ -74,20 +69,19 @@ while True:
     # Cooldown bar
     sge_rect(game_display, 700, 790, 100, 10, white)
     sge_rect(game_display, 700, 790, cooldown, 10, red)
-    
+
     bullets_temp = []
     for bullet in bullets:
-        bullets_temp.append([bullet[0]+bullet[2], bullet[1]+bullet[3], bullet[2], bullet[3]])
+        bullets_temp.append(
+            [bullet[0]+bullet[2], bullet[1]+bullet[3], bullet[2], bullet[3]])
         sge_line(game_display, black,
             (bullet[0], bullet[1]), (bullet[0]-bullet[2], bullet[1]-bullet[3]),
                  2)
     bullets = bullets_temp
     del bullets_temp
-    # print(len(bullets))
     while len(bullets) > 100:
         del bullets[0]
-        
-    
+
+
     pygame.display.update() # update
     # This should be the last thing in the loop
-
